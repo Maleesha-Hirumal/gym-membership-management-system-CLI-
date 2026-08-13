8042098831f819d1df5979c23e68dc7cb9c0d1ac
Maleesha Hirumal <h.maleeshahirumal@gmail.com>
2026-08-13 09:39:10 +0530

    up date db codes
    added sample 10 data

--- database scrypt/gym_db.sql
+++ database scrypt/gym_db.sql
@@ -28,3 +28,20 @@ INSERT INTO plans (plan_id, plan_name, duration_months, fee) VALUES
 SELECT * FROM plans;
 SELECT * FROM members;
 
+
+-- Add sample 10 data
+INSERT INTO members (member_id, name, email, phone, plan_id) VALUES
+(201, 'Chamara Rajapaksha', 'chamara.r@gmail.com', '0771234567', 1),
+(202, 'Sanduni Ekanayake', 'sanduni.e@gmail.com', '0712345678', 2),
+(203, 'Buddhika Wijesinghe', 'buddhika.w@yahoo.com', '0776543210', 3),
+(204, 'Hasini Karunaratne', 'hasini.k@gmail.com', '0754321098', 1),
+(205, 'Lasith Amarasinghe', 'lasith.a@outlook.com', '0703216549', 2),
+(206, 'Piumi Senanayake', 'piumi.s@gmail.com', '0778889990', 3),
+(207, 'Ravindu Dissanayake', 'ravindu.d@yahoo.com', '0761112233', 1),
+(208, 'Dilini Herath', 'dilini.h@gmail.com', '0724445566', 2),
+(209, 'Ashan Gunawardena', 'ashan.g@gmail.com', '0767778889', 3),
+(210, 'Nimasha Peiris', 'nimasha.p@outlook.com', '0713332221', 1);
+
+
+
+
